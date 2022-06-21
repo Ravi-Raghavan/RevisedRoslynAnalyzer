6d452687a5b88dc26ac3b0a623fe47fc4c138016
Ravi Raghavan <ravi.raghavan@mongodb.com>
2022-06-21 14:10:52 -0400

    Supports Variable Location and Reassignments

---
+++ ExampleAssembly/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace roslynTester.examples
+{
+    public class Program
+    {
+        public static void Main(string [] args)
+        {
+            int x = 10;
+            int y = 11;
+            y = x + y;
+        }
+    }
+}
